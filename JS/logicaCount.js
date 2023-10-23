e4442eec78a6b9d96551c16eb02f52062aa9ca51
Bacaj Fabio <FBacaj@siav.it>
2023-10-23 15:34:30 +0200

    Qui ce  tutta la logica che e stata implementata Con il new Date conto alla rovescia

---
+++ JS/logicaCount.js
@@ -0,0 +1,34 @@
+const indicatori = document.querySelectorAll('#time span'),
+   scadenza = new Date(2023,11,31,00,00,0),
+   ms_scadenza = scadenza.getTime(); //ms si intende per i millisecondi 
+   ms_in_giorno = 24 * 60 * 60 * 1000,
+   ms_in_ora = 60 * 60 * 1000,
+   ms_in_minuto = 60 * 1000;
+ 
+
+   function countdown(){
+    const data_corrente = new Date(),
+          ms_corrente = data_corrente.getTime(),
+          ms_rimanenti = ms_scadenza - ms_corrente;
+
+      let giorni = ~~(ms_rimanenti / ms_in_giorno),
+          ore = ~~((ms_rimanenti % ms_in_giorno)/ms_in_ora),
+          minuti = ~~((ms_rimanenti % ms_in_ora)/ms_in_minuto),
+          secondi = ~~((ms_rimanenti % ms_in_minuto)/1000);
+
+     
+     let time =[giorni,ore,minuti,secondi];
+
+     if(ms_rimanenti <=0){
+        clearInterval(countdownInterval);
+        return;
+     }
+
+     indicatori.forEach((span,index)=>{
+        span.textContent = time[index];
+     })
+          
+   }
+
+   const countdownInterval = setInterval(countdown,1000);
+   countdown();
